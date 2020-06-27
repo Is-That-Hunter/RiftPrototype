370516df5a2c17cb131bdf6d29bf55e4164b7e07
Brennan McCulloch <bmccullo@ucsc.edu>
2020-06-27 11:19:22 -0700

    Even better reeeaaallll basic jump

--- Rift Prototype/Assets/Scripts/BasicMovement.cs
+++ Rift Prototype/Assets/Scripts/BasicMovement.cs
@@ -7,7 +7,10 @@ public class BasicMovement : MonoBehaviour
     public Transform cam;
     public float speed = 10.0f;
     public float jumpHeight = 2.0f;
+    private int jumpNumber = 0;
+    public int totalJumps = 2;
     public bool playerMove = true;
+    private float distanceToGround;
     private Rigidbody body;
 
     public void LockPlayer()
@@ -19,11 +22,19 @@ public class BasicMovement : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        Collider colliderThing = GetComponent<Collider>();
+        jumpNumber = totalJumps;
+        distanceToGround = colliderThing.bounds.extents.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isGrounded())
+        {
+            jumpNumber = totalJumps;
+        }
+
         Vector3 velo = new Vector3(0, 0, 0);
         velo.y = body.velocity.y; 
         body.velocity = velo;
@@ -31,6 +42,7 @@ public class BasicMovement : MonoBehaviour
         Vector3 input = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0) * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 nothing = new Vector3(0, 0, 0);
 
+        //Player movement in a left right forward back space
         if (playerMove)
         {
             if (dPadInput != nothing)
@@ -42,5 +54,30 @@ public class BasicMovement : MonoBehaviour
                 body.MovePosition(body.position + (input * speed * Time.deltaTime));
             }
         }
+
+        //reeeaallll basic jump script
+        if (Input.GetButtonDown("PS4_X"))
+        {
+            if(isGrounded())
+            {
+                Vector3 fuckYou = body.velocity;
+                fuckYou.y += jumpHeight;
+                body.velocity = fuckYou;
+                jumpNumber--;
+            }
+            else if (jumpNumber > 0)
+            {
+                Vector3 fuckYou = body.velocity;
+                fuckYou.y += jumpHeight;
+                body.velocity = fuckYou;
+                jumpNumber--;
+            }
+        }
+
+    }
+
+    //Ground Check
+    bool isGrounded() {
+        return Physics.Raycast(body.transform.position, -Vector3.up, distanceToGround + 0.1f);
     }
 }
