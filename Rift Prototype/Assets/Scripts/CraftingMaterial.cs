ed6271007afa19d994c08ead3cc7d563403a1459
BrennanMcCulloch <mcculloch.brennan@gmail.com>
2020-07-23 10:58:25 -0700

    ok, straight up, I need a break

--- Rift Prototype/Assets/Scripts/CraftingMaterial.cs
+++ Rift Prototype/Assets/Scripts/CraftingMaterial.cs
@@ -1,42 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
+using UnityEditor;
 
 public class CraftingMaterial : MonoBehaviour
 {
-    public TextAsset tagTypes;
-
+    public TextAsset tagStuff;
+    private Tags allTags;
     // Start is called before the first frame update
     void Start()
     {
-        Tags allTags = JsonUtility.FromJson<Tags>(tagTypes.text);
-        Debug.Log(allTags);
-        foreach (var thing in allTags.size)
+        string tagDirection = AssetDatabase.GetAssetPath(tagStuff);
+        StreamReader stream = new StreamReader(tagDirection);
+        string tagJSON = stream.ReadToEnd();
+        allTags = JsonUtility.FromJson<Tags>(tagJSON);
+        Debug.Log(allTags.size.ToString());
+        foreach (Stringable thing in allTags.size)
         {
             Debug.Log("sizes: " + thing);
         }
-        foreach (var thing in allTags.type)
+        foreach (Stringable thing in allTags.type)
         {
-            Debug.Log("sizes: " + thing);
+            Debug.Log("types: " + thing);
         }
-        foreach (var thing in allTags.material)
+        foreach (Stringable thing in allTags.material)
         {
-            Debug.Log("sizes: " + thing);
+            Debug.Log("materials: " + thing);
         }
 
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
 }
 
 //Strings need to be wrapped to work with JSON. I don't understand why.
 
+[Serializable]
 public class Tags
 {
     public Stringable[] size;
@@ -44,7 +43,8 @@ public class Tags
     public Stringable[] material;
 }
 
+[Serializable]
 public class Stringable
 {
-    string thing;
+    public string thing;
 }
