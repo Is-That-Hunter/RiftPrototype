9c30e24511fb29553e78299c54dfabcceba46c3f
Parker-D-Soto <parkerdsoto@gmail.com>
2020-07-24 18:09:03 -0700

    Item crafting and database work (see description for specifics)
    I implemented a simple start for a future database. And devised a base item that can be cloned to make up the items for crafting. Including methods for selecting its attributes within the editor

---
+++ Rift Prototype/Assets/Scripts/CraftDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CraftDatabase : MonoBehaviour
+{
+    //All right lets make our selves a dictionary of what we can craft
+    //Already tested whether this will carry over to future copies of this object, and it should
+    //The goal of this script is to make it so we can easily reference the key(Three Component Types) to the value (the game Object it will refer to)
+    //Preferably we probably want to make the value the crafted game object but for the time being I'll be using Strings of their names
+
+    private void OnEnable()
+    {
+        Dictionary<(string, string, string), string> data = new Dictionary<(string, string, string), string>
+        {
+            { ("small", "electric", "metal"), "flashlight" },
+        };
+
+    }
+}
