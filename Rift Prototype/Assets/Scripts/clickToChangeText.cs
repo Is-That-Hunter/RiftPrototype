c2bbc2922cd9f51b126e5eec57f94189be4199dd
Hunter Lynch <hunterlynch123@gmail.com>
2020-09-21 16:59:21 -0700

    Core info change implemented

---
+++ Rift Prototype/Assets/Scripts/clickToChangeText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class clickToChangeText : MonoBehaviour
+{
+
+    public TextMeshProUGUI textshown = null;
+    public void changeWord (string word)
+    {
+        textshown.text = word;
+    }
+}
