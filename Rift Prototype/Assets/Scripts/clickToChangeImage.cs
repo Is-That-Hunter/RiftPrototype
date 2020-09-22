d19de6149606af07d654a542d6faa234b92c7c15
Hunter Lynch <hunterlynch123@gmail.com>
2020-09-22 14:10:08 -0700

    Base Inventory Logic Complete
    Functional Slots with description and image buttons

--- Rift Prototype/Assets/Scripts/clickToChangeImage.cs
+++ Rift Prototype/Assets/Scripts/clickToChangeImage.cs
@@ -5,13 +5,11 @@ using UnityEngine.UI;
 
 public class clickToChangeImage : MonoBehaviour
 {
-    [SerializeField] GameObject target;
-    //public Image icon = null;
+    public Image currentIcon = null;
     public void changeImg(Sprite currentImg)
     {
-        // this is so wrong but logic is right
-        // find object and put name here
-        // finish up image transfer
-        gameObject.transform.GetChild(0).GetComponent<Image>().sprite = currentImg;
+        currentIcon.sprite = currentImg;
     }
+
 }
+
