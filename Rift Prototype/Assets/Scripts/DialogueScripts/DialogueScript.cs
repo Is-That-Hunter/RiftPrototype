760cac6efce82a9a542cd2feeac46731280ed460
Luke Harvey <Lukeharvey0226@gmail.com>
2021-02-14 06:45:24 -0800

    Dialogue is now functional! Parses dialogue from json files

---
+++ Rift Prototype/Assets/Scripts/DialogueScripts/DialogueScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DialogueScript : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject dialogueObj;
+    public GameObject dialogueText; 
+    public GameObject characterHead;
+    public GameObject mainCamera;
+    public bool isPlaying;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform[] ts = this.gameObject.transform.GetComponentsInChildren<Transform>(true);
+        foreach (Transform t in ts)
+        {
+            Debug.Log(t.gameObject.name);
+            if(t.gameObject.name == "CharacterHead")
+            {
+                this.characterHead = t.gameObject;
+            }
+            if(t.gameObject.name == "DialogueText")
+            {
+                this.dialogueText = t.gameObject;
+            }
+        }
+        changePortrait();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.GetComponent<Canvas>().enabled = isPlaying;
+        if(isPlaying) {
+            this.mainCamera.GetComponent<CameraController>().focus = true;
+            this.mainCamera.GetComponent<CameraController>().zoomIn = true;
+        }
+        string text= this.dialogueObj.GetComponent<twineParser>().getCurrText();
+        this.dialogueText.GetComponent<TMPro.TextMeshProUGUI>().text = text;
+    }
+
+    public void changePortrait() {
+        this.characterHead.GetComponent<Image>().sprite = this.dialogueObj.GetComponent<twineParser>().getCurrPortrait();
+        this.mainCamera.GetComponent<CameraController>().focusObject = this.dialogueObj.GetComponent<twineParser>().getCurrCharacterFocus().transform;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        TMP_Text pTextMeshPro = this.dialogueText.GetComponent<TMP_Text>();
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(pTextMeshPro, eventData.position, null);  // If you are not in a Canvas using Screen Overlay, put your camera instead of null
+        if (linkIndex != -1) { // was a link clicked?
+            TMP_LinkInfo linkInfo = pTextMeshPro.textInfo.linkInfo[linkIndex];
+            var linkId = linkInfo.GetLinkID();
+            bool leave = this.dialogueObj.GetComponent<twineParser>().chooseOption(linkId);
+            changePortrait();
+            if(leave)
+            {
+                this.mainCamera.GetComponent<CameraController>().focus = false;
+                this.mainCamera.GetComponent<CameraController>().zoomIn = false;
+                isPlaying = false;
+            }
+        }
+    }
+}
---
+++ Rift Prototype/Assets/Scripts/DialogueScripts/twineParser.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+[System.Serializable]
+public class twine
+{
+    public passage[] passages;
+}
+[System.Serializable]
+public class passage
+{
+    public string character;
+    public string parsedText;
+    public string text;
+    public links[] links;
+    public string name;
+    public string pid;
+}
+[System.Serializable]
+public class links
+{
+    public bool leave;
+    public string name;
+    public string link;
+    public string pid;
+}
+[System.Serializable]
+public class dialogueVariables
+{
+    public string name;
+    public bool isActive;
+}
+[System.Serializable]
+public class characterPortraits
+{
+    public string name;
+    public Sprite portrait;
+    public GameObject character;
+}
+public class twineParser : MonoBehaviour
+{
+    private twine dialogueTree;
+    public TextAsset dialogueJson;
+    public int currPid;
+    public dialogueVariables[] vars;
+    public GameObject mainCamera;
+    public characterPortraits[] characters;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FromJson();
+    }
+
+    void FromJson() 
+    {
+        string dialoguePath = AssetDatabase.GetAssetPath(dialogueJson);
+        using (StreamReader r = new StreamReader(dialoguePath))
+        {
+            string json = r.ReadToEnd();
+            twine dialogue = JsonUtility.FromJson<twine>(json);
+            this.dialogueTree = dialogue;
+        }
+        foreach(passage p in this.dialogueTree.passages)
+        {
+            p.parsedText = this.formatTextForClickable(p.text);
+        }
+
+    }
+    public passage getCurrPassage() {
+        foreach(passage p in this.dialogueTree.passages)
+        {
+            if(Int32.Parse(p.pid)==this.currPid)
+                return p;
+        }
+        return null;
+    }
+    public characterPortraits getCurrCharacterPortraits() {
+        passage p = getCurrPassage();
+        foreach(characterPortraits charPortrait in this.characters)
+        {
+            if(p.character == charPortrait.name)
+                return charPortrait;
+        }
+        return null;
+    }
+    public Sprite getCurrPortrait() {
+        return getCurrCharacterPortraits().portrait;
+    }
+    public GameObject getCurrCharacterFocus() {
+        return getCurrCharacterPortraits().character;
+    }
+
+    public string getCurrText() {
+        return this.getCurrPassage().parsedText;
+    }
+
+    public bool chooseOption(string option) {
+        links[] options = this.getCurrPassage().links;
+        links choice = options.FirstOrDefault(i=>i.link == option);
+        if (choice != null)
+        {
+            this.currPid = Int32.Parse(choice.pid);
+            return choice.leave;
+        }
+        return false;
+    }
+    //index 0 = name, index 1 = link
+    //returns name and link from link in text
+    private string[] getNameAndLink(string parseThis)
+    {
+        if(parseThis.Contains("->"))
+            return parseThis.Split(new string[]{"->"}, StringSplitOptions.None);
+        string[] ret = {parseThis,parseThis};
+        return ret;
+    }
+    //compares variable in text to see if it exists and is true
+    private bool compareStringAndVariable(string parseThis)
+    {
+        dialogueVariables var = this.vars.FirstOrDefault(i=>i.name == parseThis);
+        if(var != null)
+            return var.isActive;
+        return false;
+    }
+    //formats links so that they link to something and are colored correctly
+    //If an variable option exists color it red
+    private string linkToHTML(string[] parseThis, bool isVar = false)
+    {
+        string color = "blue";
+        if(isVar)
+            color = "red";
+        string temp = "<link=\""+parseThis[1]+"\"><color="+color+">"+parseThis[0]+"</color></link>";
+        return temp;
+    }
+    public string formatTextForClickable(string text)
+    {
+        string[] textArr = text.Split('\n');
+        Debug.Log(textArr.Length);
+        List<string> textLinks = new List<string>();
+        string ret = "";
+        for(int i = 0; i < textArr.Length; i++) {
+            //For If Statements in Text
+            if(textArr[i].Contains("(if:"))
+            {
+                //Get Indexes
+                int ifStart = textArr[i].IndexOf("(if:");
+                int ifEnd = textArr[i].IndexOf(")");
+                int linkStart = textArr[i].IndexOf("[[[");
+                int linkEnd = textArr[i].IndexOf("]]]");
+                //Get Strings
+                string fullLink = textArr[i].Substring(linkStart+3,linkEnd-linkStart-3);
+                string varName = textArr[i].Substring(ifStart+4,ifEnd-ifStart-4);
+                //Parse If and String
+                string[] nameAndLink = this.getNameAndLink(fullLink);
+                bool isActive = compareStringAndVariable(varName);
+                if(isActive) {
+                    ret += this.linkToHTML(nameAndLink);
+                }
+            }
+            //For Links in Text
+            else if(textArr[i].Contains("[["))
+            {
+                //Get Indexes
+                int linkStart = textArr[i].IndexOf("[[");
+                int linkEnd = textArr[i].IndexOf("]]");
+                //Get Strings
+                string fullLink = textArr[i].Substring(linkStart+2,linkEnd-linkStart-2);
+                string[] nameAndLink = this.getNameAndLink(fullLink);
+                ret += this.linkToHTML(nameAndLink);
+            }
+            //For Text
+            else
+            {
+                ret += textArr[i];
+            }
+            //Add NewLine unless last line
+            ret += "\n";
+        }
+        return ret;
+    }
+
+    public void changePid(int pid) {
+        this.currPid = pid;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
